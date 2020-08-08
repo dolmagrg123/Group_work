ab3ebc0f88ab4fa8e9873cec119abba0b9f97d3c
dolmagrg1231 <dolmagurung75@gmail.com>
2020-08-07 20:58:34 -0400

    Data_Object and Manager_Object class added

--- manager.py
+++ manager.py
@@ -1,1 +1,11 @@
 ## This class will be have CRUD methods
+## Data Object and manager object added
+
+class Data_Object:
+    def __init__(self):
+        pass
+
+class Manager_Object:
+    def __init__(self):
+        pass
+
